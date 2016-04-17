4b46ae860746deab90022938e8384fa5190a3947
Chris Kokolios <kokolios1986@yahoo.gr>
2016-04-17 19:11:43 +0300

    refactor poll routes

---
+++ routes/polls.js
@@ -0,0 +1,46 @@
+var express = require('express'),
+	router 	= express.Router(),
+	Poll 	= require('../models/poll');
+
+router.get('/', function(req, res) {
+
+    Poll.find({}, function(err, allPolls) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render('polls/index', { polls: allPolls });
+        }
+    });
+});
+router.get('/new', function(req, res) {
+    res.render('polls/new');
+});
+router.post('/', function(req, res) {
+
+    var pollName = req.body.name;
+    var formOptions = req.body.option;
+
+    var options = [];
+    formOptions.forEach(function(opt) {
+        options.push({
+            name: opt,
+            count: 0
+        });
+    });
+    newPoll = {
+        name: pollName,
+        options: options
+    };
+    Poll.create(newPoll, function(err, justCreated) {
+        if (err) {
+            console.log(err);
+
+        } else {
+            res.redirect('/');
+        }
+    });
+});
+
+
+
+module.exports = router;
