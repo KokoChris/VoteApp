6420d3ba485ed55e4f6cb18aa8fb7d3d4da8c7d9
kokoChris <kokolios1986@yahoo.gr>
2016-04-19 14:07:24 +0300

    use google charts

--- public/customjs/newoption.js
+++ public/customjs/newoption.js
@@ -1,5 +1,7 @@
 var optionHtml = '<div class="form-group">' +
     '<input class="form-control" type="text" name="option" placeholder="poll option"></input>' + '<i class="fa fa-trash" aria-hidden="true"></i>' + '</div>';
+
+ 
 $(document).ready(function() {
     $(".btn-warning").on('click', function() {
         $('.btn-primary').closest('.form-group').prepend(optionHtml);
@@ -8,6 +10,5 @@ $(document).ready(function() {
     $('.form-group').on('click', '.fa-trash', function() {
         $(this).closest('.form-group').remove();
     });
-
-    
+   
 });
